2aa6c45009d9d8f7cabcb4253be98af4edb26db4
VykeFanatic <mc3789cm@protonmail.com>
2025-09-02 15:18:19 +0000

    Update logging_labels.py

--- ArchonCode/logging_labels.py
+++ ArchonCode/logging_labels.py
@@ -9,10 +9,10 @@ INFO_LOG = "[ \033[0;32mINFO\033[0m ]"
 """Use in routine information messages."""
 
 WARN_LOG = "[ \033[0;33mWARN\033[0m ]"
-"""Use when an unexpected event and/or argument is passes, but it’s not stopping the continuation of any functions."""
+"""Use when an unexpected event and/or argument is passed, but it’s not stopping the continuation of the function(s)."""
 
 EROR_LOG = "[ \033[0;31mEROR\033[0m ]"
-"""Use when a function fails and can’t continue properly."""
+"""Use when a function fails and can’t continue properly. If possible, rollback changes, if any, for better redundancy. """
 
 CRIT_LOG = "[ \033[1;31mCRIT\033[0m ]"
 """Use when an event/argument would result in the complete stoppage of Archon."""
